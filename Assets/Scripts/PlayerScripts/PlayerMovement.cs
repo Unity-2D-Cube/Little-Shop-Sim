f440e166bbfb519b9ed977a3d0d3b6e455ef4617
Unity-2D-Cube <49322919+Unity-2D-Cube@users.noreply.github.com>
2023-06-08 18:54:19 +0200

    Added Player Animations while Player moves
    Added Player Move and Idle Animations with Blend Trees.

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -7,24 +7,23 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float playerSpeed;
 
-    //[SerializeField] private float rotationSpeed;
 
     private Rigidbody2D rb2D;
     private Vector2 movementInput;
 
-    //private Vector2 smoothedMovementInput;
-    //private Vector2 movementInputSmoothVelocity;
+    private Animator animator;
+
 
     private void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         movePayer();
-        //SetPlayerVelocity();
-        //RotateInDirectionOfInput();
+
     }
 
     private void movePayer()
@@ -32,33 +31,27 @@ public class PlayerMovement : MonoBehaviour
         rb2D.MovePosition(rb2D.position + movementInput * playerSpeed * Time.fixedDeltaTime);
     }
 
-    //private void SetPlayerVelocity()
-    //{
-    //    smoothedMovementInput = Vector2.SmoothDamp(
-    //        smoothedMovementInput,
-    //        movementInput,
-    //        ref movementInputSmoothVelocity,
-    //        0.1f);
 
-    //    rb2D.velocity = smoothedMovementInput * playerSpeed;
-    //}
-
-    //private void RotateInDirectionOfInput()
-    //{
-    //    if(movementInput != Vector2.zero)
-    //    {
-    //        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, smoothedMovementInput);
-    //        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-
-    //        rb2D.MoveRotation(rotation);
-
-    //    }
-    //}
 
 
     private void OnMove(InputValue inputValue)
     {
         movementInput = inputValue.Get<Vector2>();
+
+        if(movementInput.x != 0 || movementInput.y != 0)
+        {
+            
+
+            animator.SetFloat("X", movementInput.x);
+            animator.SetFloat("Y", movementInput.y);
+
+            animator.SetBool("IsWalking", true);
+        }
+        else {
+            animator.SetBool("IsWalking", false);
+        }
+
+
     }
 
 } // Class
